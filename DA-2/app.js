5bc6f8917e6bdf20b6e9d24ae00557497f9bc7ec
mannsk1302 <138506116+mannsk1302@users.noreply.github.com>
2025-09-26 18:46:49 +0530

    Add file upload functionality with `/test` view and multer configuration.

--- DA-2/app.js
+++ DA-2/app.js
@@ -5,6 +5,22 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const userModel = require('./models/user');
 const postModel = require('./models/post');
+const crypto = require('crypto');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './public/images/uploads')
+  },
+  filename: function (req, file, cb) {
+    crypto.randomBytes(12, (err, bytes) => {
+        const fn = bytes.toString("hex") + path.extname(file.originalname);
+        cb(null, fn)
+    })
+  }
+})
+
+const upload = multer({ storage: storage })
 
 const app = express();
 
@@ -18,6 +34,14 @@ app.use(cookieParser());
 // ================= HOME =================
 app.get('/', (req, res) => {
     res.render('index');
+});
+
+app.get('/test', (req, res) => {
+    res.render('test');
+});
+
+app.post('/upload', upload.single("image"), (req, res) => {
+    console.log(req.file);
 });
 
 // ================= REGISTER =================
